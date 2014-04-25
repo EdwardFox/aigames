915cbb714886c08b0be95e2bf622f7c076f76965
Patrick Bauer <patrick.jean.bauer@googlemail.com>
2014-04-25 12:27:36 +0200

    Gitignore + Resolution

--- Game.cpp
+++ Game.cpp
@@ -3,7 +3,7 @@
 const sf::Time Game::TimePerFrame = sf::seconds(1.f/60.f);
 
 Game::Game() :
-mWindow(sf::VideoMode(1680, 1050, 32), "BlockKeeper")
+mWindow(sf::VideoMode(640, 380, 32), "BlockKeeper")
 , mWorld()
 , mPlayer()
 {
